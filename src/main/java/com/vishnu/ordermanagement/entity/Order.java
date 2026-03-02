9047322a45fdaf1596eca20c12b36c1825469733
vishnu-java-dev <mvishnuvardhan9494@gmail.com>
2026-03-02 22:00:24 +0530

    Initial Project Setup With Springboot Configuration

--- src/main/java/com/vishnu/ordermanagement/entity/Order.java
+++ src/main/java/com/vishnu/ordermanagement/entity/Order.java
@@ -13,7 +13,6 @@ public class Order {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-
     private String customerName;
     private Double amount;
     private String status;
